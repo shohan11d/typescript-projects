b7e572d98a4e13ec82b882f4f62a20f264771be5
shohan11d <shohan11d@gmail.com>
2024-12-17 18:14:33 +0600

    function type started

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,10 +1,11 @@
-let bike: { brand: string; year: number } = { brand: 'yamaya', year: 2010 };
-let laptop: { brand: string; year: number } = { brand: 'Dell', year: 2020 };
-// let laptop2:{brand:string, year:number} = {brand:'hp' }
+function calculateDiscount(price: number): number {
+   const hasDiscount = true;
+   if (hasDiscount) {
+      return 9;
+   }
+   return price * 0.9;
+}
 
-let product1 = { title: 'shirt', price: 20 };
-let product2 = { title: 'pants' };
+const finalPrice = calculateDiscount(200);
 
-let products: { title: string; price?: number }[] = [product1, product2];
-
-products.push({ title: 'shoes' });
+console.log(finalPrice);
