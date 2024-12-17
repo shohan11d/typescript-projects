af3f1b4dbf2d78b6cb624df76382f7c3eb97031d
shohan11d <shohan11d@gmail.com>
2024-12-17 15:45:12 +0600

    challenge 3 done

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,19 +1,3 @@
-const books = [
-   '1984',
-   'Brave New World',
-   'Angel',
-   'Fahrenheit 451',
-   'The Martian',
-];
+const temperatures: number[] = [12, 434, -1];
 
-let foundBook: string | undefined ;
-
-for (let book of books) {
-   if (book === '1984') {
-      foundBook = book;
-      foundBook = foundBook.toUpperCase();
-      break;
-   }
-}
-
-console.log(foundBook?.length);
+const mixArray: (number | string)[] = ['dfj', 'df', 13];
