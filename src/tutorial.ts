7a3ff509d25a6b1d7d56d3b611c7a1fcae2cf73c
shohan11d <shohan11d@gmail.com>
2024-12-17 17:26:42 +0600

    objects added

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,3 +1,10 @@
-const temperatures: number[] = [12, 434, -1];
+let bike: { brand: string; year: number } = { brand: 'yamaya', year: 2010 };
+let laptop: { brand: string; year: number } = { brand: 'Dell', year: 2020 };
+// let laptop2:{brand:string, year:number} = {brand:'hp' }
 
-const mixArray: (number | string)[] = ['dfj', 'df', 13];
+let product1 = { title: 'shirt', price: 20 };
+let product2 = { title: 'pants' };
+
+let products: { title: string; price?: number }[] = [product1, product2];
+
+products.push({ title: 'shoes' });
