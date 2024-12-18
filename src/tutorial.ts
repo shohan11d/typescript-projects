a639ae65c50e0870891baf2f6c7de83bd4f77f81
shohan11d <shohan11d@gmail.com>
2024-12-18 14:19:05 +0600

    processdata function done

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,9 +1,28 @@
-const array: string[] = ['shohan', 'rakib', 'emon', 'tanver'];
+// const processUnit = function (unit: string | number) {
+//       unit = unit * 2;
+//       console.log(`Number * 2 = ${unit}`);
+//    } else console.log(`Sring: ${unit}`);
+// };
+// processUnit('SUPP');
+//    if (typeof unit === 'number') {
 
-const validator = function (input: string[]): boolean {
-   const result: string | undefined = input.find((item) => item === 'shohan');
-   const output: boolean = result ? true : false;
-   return output;
+// alternative
+// function createStudent(student: { id: number; name: string }): void {
+//    console.log(`Welcome to the course ${student.name.toUpperCase()}!!!`);
+// }
+
+// createStudent({ id: 1, name: 'john' });
+
+const processData = function (
+   input: string | number,
+   config: { reverse: boolean }
+): string | number | undefined {
+   if (typeof input === 'number') return input * 2;
+   if (typeof input === 'string' && config.reverse !== true)
+      return input.toUpperCase();
+   if (config.reverse === true && typeof input === 'string')
+      return input.split('').reverse().join('');
 };
 
-console.log(validator(array));
+console.log(processData('shohan', { reverse: false }));
+console.log(processData('Rakib', { reverse: true }));
